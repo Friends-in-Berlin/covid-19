eef94af8a12708a1219409eea03aad5bcb2ed2f5
Yasmin <yasminefawzy22@gmail.com>
2020-04-13 22:37:52 +0200

    retrieving data

--- src/App.js
+++ src/App.js
@@ -1,30 +1,61 @@
-import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Home from './Components/home/Home';
-import News from './Components/news/News';
-import Protection from './Components/protection/Protection';
-import Warning from './Components/warning/Warning';
-import Support from './Components/support/Support';
-import Nav from './Components/navbar/Nav';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "./Components/home/Home";
+import News from "./Components/news/News";
+import Protection from "./Components/protection/Protection";
+import Warning from "./Components/warning/Warning";
+import Support from "./Components/support/Support";
+import Nav from "./Components/navbar/Nav";
+import "./App.css";
+//import axios from "axios";
 
-function App() {
-  return (
-    <BrowserRouter>
+export class App extends Component {
+  state = {
+    
+    /* news: [], */
+  };
 
-      <div className="App">
-        <Nav />
-        <Switch>
-          <Route path='/' component={Home} exact />
-          <Route path='/news' component={News} />
-          <Route path='/protection' component={Protection} />
-          <Route path='/warning' component={Warning} />
-          <Route path='/support' component={Support} />
-        </Switch>
+ 
 
-      </div>
-    </BrowserRouter>
-  );
+  /* NEW API  */
+
+  /* fetchNews = async () => {
+    const response = await axios.get(
+      "http://covid19-news.herokuapp.com/api/covid19/news"
+    );
+    this.setState({
+      news: response,
+    });
+    console.log("News data", response);
+  }; */
+
+  componentDidMount() {
+   
+    /* this.fetchNews(); */
+  }
+
+  render() {
+   
+
+    return (
+      <BrowserRouter>
+       
+
+        <div className="App">
+          <Nav />
+          <Switch>
+            <Route path="/" component={Home} exact />
+            <Route path="/news" component={News} />
+            <Route path="/protection" component={Protection} />
+            <Route path="/warning" component={Warning} />
+            <Route path="/support" component={Support} />
+          </Switch>
+        </div>
+       
+      </BrowserRouter>
+     
+    );
+  }
 }
 
 export default App;
--- src/Components/news/News.js
+++ src/Components/news/News.js
@@ -1,14 +1,34 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
+import axios from "axios";
 
 export class News extends Component {
-    render() {
-        return (
-            <div>
+  state = {
+    quest: [],
+  };
 
-            </div>
-        )
-    }
+  componentDidMount() {
+    axios
+      .get(`http://covid19-news.herokuapp.com/api/covid19/faqs`)
+      .then((res) => {
+        const quest = res.data;
+        console.log(quest);
+        this.setState({ quest });
+      });
+  }
+  render() {
+    return (
+      <div>
+        {this.state.quest.map((item, index) => {
+          return (
+            <ul key={index}>
+              <li>{item.question}</li>
+              <li>{item.answer}</li>
+            </ul>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
-export default News
+export default News;
